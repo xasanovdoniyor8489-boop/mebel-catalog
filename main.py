35ae0bbf73b37c8bd4dfbe089e5de5b7287d4a3a
Doniyor <xasanovdoniyor8489@gmail.com>
2026-04-29 18:56:35 +0500

    fix

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"])
 
 try:
-    app.mount("/static", StaticFiles(directory="static"), name="static")
+    
 except Exception:
     pass
 
